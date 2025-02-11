fe1c62cb92b901071b778ba7717fc545a8971db5
keezeem <azeemolajide7@gmail.com>
2025-02-11 15:21:17 +0100

    Product & controler

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express, { json, urlencoded, Request } from 'express';
+
+import productsRoutes from './routes/products/index'
+
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/products', productsRoutes);
+
+app.listen(port, () => { 
+    console.log(`Example app listening on port ${port}`);
+  });
+
---
+++ api/src/routes/products/productsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+
+
+export function listProducts(req: Request, res: Response) {
+    res.send('listProduct')
+}
+
+export function getProductById(req: Request, res: Response) {
+    res.send('getProductById')
+}
+
+export function deleteProduct(req: Request, res: Response) {
+    res.send('createProduct')
+}
+
+export function createProduct(req: Request, res: Response) {
+    res.send('updateProduct')
+}
+
+export function updateProduct(req: Request, res: Response) {
+    res.send('deleteProduct')
+}
