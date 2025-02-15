db5c738e6a00bf03d696dea192a4cc4681a4886f
keezeem <azeemolajide7@gmail.com>
2025-02-15 12:37:59 +0100

    product CRUD

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { json, urlencoded, Request } from 'express';
+
+import productsRoutes from './routes/products/index'
+
+
+const port = 3000;
+const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/products', productsRoutes);
+
+app.listen(port, () => { 
+    console.log(`Example app listening on port ${port}`);
+  });
+
