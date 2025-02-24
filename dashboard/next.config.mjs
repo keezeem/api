48c4fa22a0d9d2e477c3bf764d2fcc46623eef82
keezeem <azeemolajide7@gmail.com>
2025-02-24 09:23:43 +0100

    layouyt, navigation, sidebar

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,7 +1,34 @@
 import { withGluestackUI } from "@gluestack/ui-next-adapter";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["nativewind", "react-native-css-interop"]
+  transpilePackages: [
+    "nativewind",
+    "react-native-css-interop",
+    "@gluestack-ui/avatar",
+    "@gluestack-ui/themed",
+    "@gluestack-style/react",
+    "react-native-svg"
+  ],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "react-native$": "react-native-web"
+    };
+
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      include: /node_modules\/@gluestack-ui/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["next/babel"]
+        }
+      }
+    });
+
+    return config;
+  }
 };
 
 export default withGluestackUI(nextConfig);
