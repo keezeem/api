d3c3630c5662b669619476ed3d1cdeb35a242122
keezeem <azeemolajide7@gmail.com>
2025-02-25 23:43:41 +0100

    orders details

--- dashboard/app/dashboard/orders/page.tsx
+++ dashboard/app/dashboard/orders/page.tsx
@@ -4,17 +4,20 @@ import { Card } from '@/components/ui/card';
 import { HStack } from '@/components/ui/hstack';
 import { Text } from '@/components/ui/text';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 
 export default async function OrdersPage() {
   const orders = await fetchOrders();
 
   return (
     <Card className="w-full max-w-screen-lg">
-      <HStack className="p-4 border-b border-gray-200 gap-4">
-        <Text className="font-bold">Id</Text>
-        <Text className="font-bold">Date</Text>
-        <Text className="ml-auto font-bold">Status</Text>
-      </HStack>
+    <Link href={`/dashboard/orders/${orders.id}`} key={orders.id}>
+          <HStack className="p-4 border-b border-gray-200 gap-4">
+            <Text>{orders.id}</Text>
+            <Text>{dayjs(orders.createdAt).format('DD/MM/YYYY HH:mm')}</Text>
+            <Text className="ml-auto">{orders.status}</Text>
+          </HStack>
+        </Link>
 
       {orders.map((order) => (
         <HStack key={order.id} className="p-4 border-b border-gray-200 gap-4">
